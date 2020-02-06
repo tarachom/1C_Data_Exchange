ad3ae94f5a2cd6b7f450594fc75b4e9ff66073fc
Tarchom Tarik <tarachom@i.ua>
2020-02-06 21:25:03 +0200

    up

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ using System.Threading;
 using System.Net;
 using System.Web;
 
+using System.Xml.Xsl;
+
 namespace _1C_Data_Exchange
 {
 	class Program
@@ -16,11 +18,7 @@ namespace _1C_Data_Exchange
 		public static string pathSaveXMLCommand { get; set; }
 		public static string pathReadXMLResult { get; set; }
 
-		public static object locker;
-
 		public static List<HttpListenerContext> list { get; set; }
-
-
 
 		static void Main(string[] args)
 		{
@@ -34,8 +32,6 @@ namespace _1C_Data_Exchange
 
 			Thread thread = new Thread(new ThreadStart(Test));
 			thread.Start();
-
-			
 		}
 
 		static void WebServer()
@@ -59,7 +55,7 @@ namespace _1C_Data_Exchange
 				}
 			}
 
-			listener.Stop();
+			//listener.Stop();
 		}
 
 		static void Test()
@@ -80,11 +76,13 @@ namespace _1C_Data_Exchange
 				if (context != null)
 				{
 					HttpListenerRequest request = context.Request;
-					
+
+					Console.WriteLine("-------------->");
+
 					foreach (string key in request.QueryString.AllKeys)
 					{
 						Console.WriteLine(key + " = " + request.QueryString[key].ToString());
-					}					
+					}
 
 					if (request.HttpMethod == "POST")
 					{
@@ -105,27 +103,57 @@ namespace _1C_Data_Exchange
 						{
 							string[] kvPair = param.Split('=');
 							string key = kvPair[0];
-							string value =  HttpUtility.UrlDecode(kvPair[1]);
+							string value = HttpUtility.UrlDecode(kvPair[1]);
 							postParams.Add(key, value);
 							Console.WriteLine(key + " = " + value);
 						}
 					}
 
-					
+					string guid = Guid.NewGuid().ToString();
+					string cmd = request.QueryString["cmd"];
+
+					Save(guid, cmd);
 
 					HttpListenerResponse response = context.Response;
-
-					string responseString =
-						"<HTML><BODY>" + "" +
-					    "<form method=\"post\">First name: <input type=\"text\" name=\"firstname\" /><br />Last name: <input type=\"text\" name=\"lastname\" /><input type=\"submit\" value=\"Submit\" /></form>" +
-					    "</BODY></HTML>";
-
-					byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-
-					response.ContentLength64 = buffer.Length;
 					Stream output = response.OutputStream;
-					output.Write(buffer, 0, buffer.Length);
-					output.Close();
+
+					string pathToRead = pathReadXMLResult + guid + ".xml";
+					int timeout = 8000;
+
+					Console.WriteLine("start timeout " + DateTime.Now.ToString());
+
+					while (timeout > 0)
+					{
+						if (File.Exists(pathToRead))
+						{
+							Read(pathToRead, output);
+							
+
+							Console.WriteLine("Ok");
+
+							File.Delete(pathToRead);
+
+							break;
+						}
+						else
+						{
+							timeout -= 400;
+
+							Console.WriteLine(DateTime.Now.TimeOfDay.ToString());
+							Thread.Sleep(50);
+						}
+					}
+					
+					Console.WriteLine("end timeout " + DateTime.Now.ToString());
+
+					try
+					{
+						output.Close();
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.Message);
+					}
 				}
 
 				//Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
@@ -133,16 +161,25 @@ namespace _1C_Data_Exchange
 			}
 		}
 
-
-
-		static void Save()
+		static void Read(string pathToRead, Stream output)
 		{
+			XslCompiledTransform xslCompiledTransform = new XslCompiledTransform();
+			xslCompiledTransform.Load(@"../../first.xslt");
+
+			XsltArgumentList xsltArgumentList = new XsltArgumentList();
+
+			xslCompiledTransform.Transform(pathToRead, xsltArgumentList, output);
+		}
+
+		static void Save(string guid, string cmd)
+		{
+
 			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
 			xml += "<root \n" +
 				"datetime=\"" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + "\" \n" +
-				"guid=\"" + Guid.NewGuid() + "\" \n" +
-				"fileback=\"" + Guid.NewGuid() + "\" \n" +
-				"cmd=\"" + "test" + "\" \n" +
+				"guid=\"" + guid + "\" \n" +
+				"fileback=\"" + guid + "\" \n" +
+				"cmd=\"" + cmd + "\" \n" +
 				">\n";
 
 			xml += "<var name=\"var1\"><![CDATA[text]]></var>\n";
@@ -150,7 +187,7 @@ namespace _1C_Data_Exchange
 
 			xml += "</root>\n";
 
-			File.WriteAllText(@"C:\1C_XML\Input\" + Guid.NewGuid() + ".xml", xml);
+			File.WriteAllText(pathSaveXMLCommand + guid + ".xml", xml);
 		}
 	}
 }
---
+++ first.xslt
@@ -0,0 +1,38 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="html" indent="yes"/>
+
+  <xsl:template match="/">
+
+    <html>
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      </head>
+      <body>
+
+        <table>
+
+          <xsl:for-each select="root/row">
+
+            <tr>
+              <td>
+                <xsl:value-of select="name"/>
+              </td>
+              <td>
+                <xsl:value-of select="code"/>
+              </td>
+              <td>
+                <xsl:value-of select="desc"/>
+              </td>
+            </tr>
+
+          </xsl:for-each>
+
+        </table>
+
+      </body>
+    </html>
+
+  </xsl:template>
+
+</xsl:stylesheet>
